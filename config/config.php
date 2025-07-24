811a4025bcd92ad26a5d40d1367f033dcdf9849e
DavidIncarbone <david.incarbone@gmail.com>
2025-07-24 10:30:46 +0200

    create scaffolding with db connection and autoload for classes

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('SITE_NAME', 'php-personal-framework');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'Dekithedragon.91');
+define('DB_NAME', 'php_personal_framework');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'veranius/php-personal-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd1acf3e4f9c55ce2885387d889df954d4be8d628',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'veranius/php-personal-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd1acf3e4f9c55ce2885387d889df954d4be8d628',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
